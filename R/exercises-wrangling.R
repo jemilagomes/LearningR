e943724970c9e25dd9174b94b155967e274bfaee
Jemila Peter Gomes <jpgo@forens.au.dk>
2021-06-16 12:12:01 +0200

    exercise with select funtions

--- R/exercises-wrangling.R
+++ R/exercises-wrangling.R
@@ -13,3 +13,17 @@ summary(NHANES)
 
 # Look over the dataset documentation
 ?NHANES
+
+nhanes_small %>%
+    select(tot_chol, bp_sys_ave, poverty)
+
+
+nhanes_small %>%
+    rename(diabetes_diagnosis_age = diabetes_age)
+
+nhanes_small %>%
+    select(bmi, contains("age"))
+
+nhanes_small %>%
+    select(phys_active_days, phys_active) %>%
+    rename(days_phys_active = phys_active_days)
